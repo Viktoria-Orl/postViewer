c26aed7afe91e145f419d244e22217fcf9244c38
Victoria Orleanova <Vika9_96@mail.ru>
2025-08-28 18:21:34 +0300

    refactor: wrap MainLayout with React.Fragment

--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -1,13 +1,14 @@
 import Footer from "../../widgets/LayoutFooter/Footer";
 import Header from "../../widgets/LayoutHeader/Header";
 import PostList from "../../widgets/PostList/PostList";
+import { Fragment } from 'react';
 
 export default function MainLayout() {
   return (
-    <div>
+    <Fragment>
       <Header></Header>
       <PostList></PostList>
       <Footer></Footer>
-    </div>
+    </Fragment>
   );
 }
