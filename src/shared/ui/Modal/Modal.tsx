5cc074f721fbcd49beaa840e613679bf2e8f87c6
Victoria Orleanova <Vika9_96@mail.ru>
2025-09-04 22:52:09 +0300

    feat: implement modal window rendering using compound components

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,35 +1,52 @@
 import { createPortal } from "react-dom";
-import { Button } from "../Button/Button";
 import styles from "./Modal.module.css";
 import { useTheme } from "../../lib/theme/useTheme";
 import type { FC } from "react";
 
 type ModalProps = {
   isOpen: boolean;
-  onClose: () => void;
-  title?: string;
+  closeModal: () => void;
   children: React.ReactNode;
 };
 
-export const Modal: FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
+export const Modal: FC<ModalProps> & {
+  Header: FC<{ children: React.ReactNode }>;
+  Body: FC<{ children: React.ReactNode }>;
+  Footer: FC<{ children: React.ReactNode }>;
+} = ({ isOpen, closeModal, children }) => {
   const modalRoot = document.getElementById("portal-root");
   const { theme } = useTheme();
 
   if (!isOpen || !modalRoot) return null;
+  
+  console.log(styles.modalHeader); 
+  console.log(styles.modalBody);
 
   return createPortal(
-    <div className={styles.modalOverlay} onClick={onClose}>
+    <div className={styles.modalOverlay} onClick={closeModal}>
       <div
         className={`${styles.modalContent} ${styles[theme]}`}
         onClick={(e) => e.stopPropagation()}
       >
-        <div className={styles.modalHeader}>
-          <Button onClick={onClose}>❌</Button>
-          {title && <h2>{title}</h2>}
-        </div>
-        <div className={styles.modalBody}>{children}</div>
+        {children}
       </div>
     </div>,
     modalRoot,
   );
 };
+
+export const ModalHeader: FC<{ children: React.ReactNode }> = ({ children }) => (
+  <div className={styles.modalHeader}>{children}</div>
+);
+
+export const ModalBody: FC<{ children: React.ReactNode }> = ({ children }) => (
+  <div className={styles.modalBody}>{children}</div>
+);
+
+export const ModalFooter: FC<{ children: React.ReactNode }> = ({ children }) => (
+  <div className={styles.modalFooter}>{children}</div>
+);
+
+Modal.Header = ModalHeader;
+Modal.Body = ModalBody;
+Modal.Footer = ModalFooter;
--- src/widgets/LayoutHeader/Header.tsx
+++ src/widgets/LayoutHeader/Header.tsx
@@ -5,11 +5,36 @@ import styles from "./Header.module.css";
 import { Button } from "../../shared/ui/Button/Button";
 import { Modal } from "../../shared/ui/Modal/Modal";
 
+const modalContent = (
+  <>
+    <p>
+      Приложение для просмотра постов и комментариев Post viewer app, создано в
+      рамках учебного проекта.
+    </p>
+    <p>
+      В рамках <b>домашнего задания 3</b> реализовано:
+    </p>
+    <p>
+      ✅ Код организован по FSD-архитектуре, для стилизации компонентов
+      используются CSS Modules
+    </p>
+    <p>
+      ✅ Посты отображаются списком с использованием React.Fragment и ключей
+    </p>
+    <p>✅ Настроено переключение темы (светлая/тёмная) через ThemeContext</p>
+    <p>
+      ✅ Добавлено модальное окно через React.Portal, которое открывается из
+      шапки
+    </p>
+  </>
+);
+
 export const Header: FC = () => {
   const { theme } = useTheme();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
 
   return (
     <header className={`${styles.header} ${styles[theme]}`}>
@@ -19,24 +44,21 @@ export const Header: FC = () => {
         <ThemeSwitcher></ThemeSwitcher>
       </div>
 
-      <Modal isOpen={isModalOpen} onClose={closeModal} title="О проекте">
-        <p>
-          Приложение для просмотра постов и комментариев Post viewer app,
-          создано в рамках учебного проекта.
-        </p>
-        <p>Уже реализовано:</p>
-        <p>
-          ✅Код организован по FSD-архитектуре, для стилизации компонентов
-          используются CSS Modules{" "}
-        </p>
-        <p>
-          ✅Посты отображаются списком с использованием React.Fragment и ключей
-        </p>
-        <p>✅Настроено переключение темы (светлая/тёмная) через ThemeContext</p>
-        <p>
-          ✅Добавлено модальное окно через React.Portal, которое открывается из
-          шапки
-        </p>
+      <Modal isOpen={isOpen} closeModal={closeModal}>
+        <Modal.Header>
+          <Button onClick={closeModal}>❌</Button>
+          <h2>О проекте</h2>
+        </Modal.Header>
+        <Modal.Body>{modalContent}</Modal.Body>
+        <Modal.Footer>
+          <a
+            href="https://github.com/Viktoria-Orl/postViewer/"
+            target="_blank"
+            className={styles.link}
+          >
+            GitHub проекта
+          </a>
+        </Modal.Footer>
       </Modal>
     </header>
   );
