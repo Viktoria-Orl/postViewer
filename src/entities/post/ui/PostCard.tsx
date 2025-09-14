69e7433eea23a4e3f7ab23bdb7ef4e6ef2e8f002
Victoria Orleanova <Vika9_96@mail.ru>
2025-09-14 21:56:57 +0300

    refactor(PostCard): remove unnecessary useCallback from toggleComments

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, type FC } from "react";
+import { useState, type FC } from "react";
 import type { Comment, Post } from "../model/types";
 import { useTheme } from "../../../shared/lib/theme/useTheme";
 import styles from "./PostCard.module.css";
@@ -14,9 +14,9 @@ export const PostCard: FC<PostCardProps> = ({ post, comments }) => {
   const { theme } = useTheme();
   const [isCommentOpen, setIsCommentOpen] = useState(false);
 
-  const toggleComments = useCallback(() => {
-    setIsCommentOpen((prev) => !prev);
-  }, []);
+const toggleComments = () => {
+  setIsCommentOpen((prev) => !prev);
+};
 
   const hasComments: boolean = comments.length > 0;
 
